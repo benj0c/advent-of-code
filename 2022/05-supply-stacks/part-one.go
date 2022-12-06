6b46cfdc1b63fd94679e638d3a265897b3971559
Vanja Benjocki <benjocki.vanja@gmail.com>
2022-12-06 09:41:18 +0100

    fifth day advent of code 2022 - supply stacks

---
+++ 2022/05-supply-stacks/part-one.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "strings"
+    "strconv"
+)
+
+func main() {
+    var stacks = make(map[int][]string)
+    var parseSteps bool
+    var result string
+
+    file, err := os.Open("input.txt")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        if line == "" {
+            parseSteps = true
+            continue
+        }
+
+        if !parseSteps && string(line[1]) != "1" {
+            cols := (len(line) + 1) / 4
+
+            for i := 0; i < cols; i++ {
+                crate := string(line[i * 4 + 1])
+
+                if crate != " " {
+                    stacks[i + 1] = append(stacks[i + 1], crate)
+                }
+            }
+        }
+
+        if parseSteps {
+            params := strings.Split(line, " ")
+            q, _ := strconv.Atoi(params[1])
+            from, _ := strconv.Atoi(params[3])
+            to, _ := strconv.Atoi(params[5])
+
+            temp := append([]string{}, stacks[from][:q]...)
+            stacks[from] = stacks[from][q:]
+            stacks[to] = append(reverse(temp), stacks[to]...)
+        }
+    }
+
+    for i :=  0; i < len(stacks); i++ {
+        result += stacks[i+1][0]
+    }
+
+    fmt.Println(result)
+
+    if err := scanner.Err(); err != nil {
+        fmt.Println(err)
+    }
+}
+
+func reverse(items []string) []string {
+	for i := 0; i < len(items)/2; i++ {
+		j := len(items) - i - 1
+		items[i], items[j] = items[j], items[i]
+	}
+	return items
+}
