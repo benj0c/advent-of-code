5c619cfb4552fdff94c0288679d2ce371cf47f49
Vanja Benjocki <benjocki.vanja@gmail.com>
2022-12-16 09:56:11 +0100

    seventh day advent of code 2022 - no space left on device

---
+++ 2022/07-no-space-left-on-device/part-two.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "strings"
+  "strconv"
+)
+
+type Node struct {
+  name string
+  isDir bool
+  size int
+  parent *Node
+  children map[string]*Node
+}
+
+const diskCapacity = 70000000
+const needCapacity = 30000000
+
+func main() {
+  var root = Node{ name: "/", isDir: true, children: make(map[string]*Node) }
+  var currentDir *Node
+
+  file, err := os.Open("input.txt")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+
+  for scanner.Scan() {
+    line := scanner.Text()
+
+    if strings.HasPrefix(line, "$ cd") {
+      path := strings.Split(line, " ")[2]
+
+      if (path == "/") {
+        currentDir = &root
+      } else if (path == "..") {
+        currentDir = (*currentDir).parent
+      } else {
+        if (*currentDir).children[path] != nil {
+          currentDir = (*currentDir).children[path]
+        } else {
+          newDir := Node{ name: path, isDir: true, parent: currentDir, children: make(map[string]*Node) }
+          (*currentDir).children[path] = &newDir
+          currentDir = &newDir
+        }
+      }
+    } else if line == "$ ls" {
+      continue
+    } else {
+      params := strings.Split(line, " ")
+      itemName := params[1]
+
+      var newNode Node
+
+      if params[0] == "dir" {
+        newNode = Node{ name: itemName, isDir: true, parent: currentDir, children: make(map[string]*Node) }
+      } else {
+        itemSize, _ := strconv.Atoi(params[0])
+        newNode = Node{ name: itemName, isDir: false, size: itemSize }
+      }
+
+      (*currentDir).children[params[1]] = &newNode
+    }
+  }
+
+  calculateDirSize(&root)
+
+  threshold := needCapacity - (diskCapacity - root.size)
+  fmt.Println(findResult(root, threshold, root.size))
+
+  if err := scanner.Err(); err != nil {
+    fmt.Println(err)
+  }
+}
+
+func findResult(node Node, threshold int, result int) int {
+  for _, item := range node.children {
+    if item.isDir {
+      result = findResult(*item, threshold, result)
+    }
+  }
+
+  if (node.size > threshold && node.size < result) {
+    result = node.size
+  }
+
+  return result
+}
+
+func calculateDirSize(node *Node) int {
+  for _, item := range node.children {
+    if item.isDir {
+      node.size += calculateDirSize(item)
+    } else {
+      node.size += item.size
+    }
+  }
+
+  return node.size
+}
